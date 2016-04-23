dce6105dc12973ac160afcc9539fb9fc08b575ca
nicholasjudd <Kcco1990>
2016-04-23 18:07:22 +1000

    React Native
    React Native Exampes

---
+++ BookSearch/SearchBooks.js
@@ -0,0 +1,27 @@
+/**
+ * Created by nicholasjudd on 19/04/2016.
+ */
+'use strict';
+
+var React = require('react-native');
+
+var {
+    StyleSheet,
+    View,
+    Component
+} = React;
+
+var styles = StyleSheet.create({
+    
+});
+
+class SearchBooks extends  Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        );
+    }
+}
+module.exports = SearchBooks;
